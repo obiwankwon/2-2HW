79029d77212a011a3f6822d2e5966cc2c4574c5b
obiwankwon <113491213+obiwankwon@users.noreply.github.com>
2025-11-06 16:01:16 +0900

    Create Homework6.java

---
+++ Homework6.java
@@ -0,0 +1,47 @@
+public class Homework6 {
+    public static void main(String[] args) {
+        OfflineCourse course1 = new OfflineCourse("HAEA9201", "Object Oriented Programming");
+        OnlineCourse course2 = new OnlineCourse("HAFL0012", "C Programming 1");
+        System.out.println (course1);
+        System.out.println (course2);
+    }
+}
+
+class Course{
+    private String code;
+    private String name;
+
+    public Course(){}
+    public Course(String code, String name){
+        this.code = code;
+        this.name = name;
+    }
+    public String getCode(){return code;}
+    public String getName(){return name;}
+}
+
+class OnlineCourse extends Course{
+
+    public OnlineCourse(){}
+    public OnlineCourse(String code, String name){
+        super(code, name);
+    }
+    @Override
+    public String toString(){
+        String msg = "Code: " + getCode() + ", Name: " + getName() + ", Type: Online";
+        return msg;
+    }
+}
+
+class OfflineCourse extends Course{
+
+    public OfflineCourse(){}
+    public OfflineCourse(String code, String name){
+        super(code, name);
+    }
+    @Override
+    public String toString(){
+        String msg = "Code: " + getCode() + ", Name: " + getName() + ", Type: Offline";
+        return msg;
+    }
+}
